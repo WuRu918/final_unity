d312538703b00d1018be36637f26f92c6031c56f
WuRu9453 <e34121298@gs.ncku.edu.tw>
2024-12-18 01:18:55 +0800

    測試第二人

--- Assets/Scripts/Model/PlatformerModel.cs
+++ Assets/Scripts/Model/PlatformerModel.cs
@@ -30,13 +30,13 @@ namespace Platformer.Model
         /// <summary>
         /// A global jump modifier applied to all initial jump velocities.
         /// </summary>
-        public float jumpModifier = 1.5f;
+        public float jumpModifier = 1.5f; 
 
         /// <summary>
         /// A global jump modifier applied to slow down an active jump when 
         /// the user releases the jump input.
         /// </summary>
-        public float jumpDeceleration = 0.5f;
+        public float jumpDeceleration = 0.4f;//I change here
 
     }
 }
