c842bb65b23e57ac93f24e76c1b491bb11d1810f
WuRu918 <wu9189453@gmail.com>
2024-12-28 19:53:14 +0800

    起始畫面

---
+++ Assets/Scripts/start/game_explaination.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class game_explaination : MonoBehaviour
+{
+    void Start(){
+
+    }
+    
+    void Updata(){
+
+    }
+
+    public void Button(){
+        print("GameStart");
+    }
+}
