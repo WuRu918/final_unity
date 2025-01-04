b4c999d99b1ea070fba3b947bb6d18d319438313
WuRu918 <wu9189453@gmail.com>
2025-01-04 21:33:11 +0800

    起始頁面音效

--- Assets/Scripts/start/game_start.cs
+++ Assets/Scripts/start/game_start.cs
@@ -1,38 +1,27 @@
 using UnityEngine;
-using System.Collections.Generic;
 using System.Collections;
-using UnityEngine.SceneManagement;  // 引入 SceneManager 命名空間
-using UnityEngine.EventSystems;  // 引入 EventSystems 用於觸發事件
+using UnityEngine.SceneManagement;
 
-public class game_explaination : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class GameExplaination : MonoBehaviour
 {
-    public RectTransform buttonRectTransform;
-    public Vector3 normalScale = new Vector3(1f, 1f, 1f);
-    public Vector3 enlargedScale = new Vector3(1.2f, 1.2f, 1f);  // 設置放大比例
+    public AudioSource audioSource; // 將 AudioSource 拖入編輯器
 
-    void Start(){
-
-    }
-    
-    void Updata(){
-
+    public void Button_to_explaination()
+    {
+        // 啟動協程
+        StartCoroutine(PlayAudioAndSwitchScene());
     }
 
-    public void Button_to_explaination(){
-        
+    private IEnumerator PlayAudioAndSwitchScene()
+    {
+        var audio = this.GetComponent<AudioSource>();
+        // 播放音效
+        audio.Play();
+
+        // 等待音效播放完成
+        yield return new WaitForSeconds(audio.clip.length);
+
+        // 切換場景
         SceneManager.LoadScene("explaination");
     }
-
-    // 當滑鼠進入按鈕時，放大
-    public void OnPointerEnter(PointerEventData eventData)
-    {
-        buttonRectTransform.localScale = enlargedScale;
-    }
-
-    // 當滑鼠離開按鈕時，恢復大小
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        buttonRectTransform.localScale = normalScale;
-    }
-    
 }
