c89e3d6566095f80bfe1e9e95d9d532906420e86
WuRu918 <wu9189453@gmail.com>
2025-01-06 00:06:23 +0800

    問答畫面
    倒數

---
+++ Assets/Scripts/npc/answer_question.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Flower;
+using UnityEngine.SceneManagement;
+using System;
+
+public class answer_question : MonoBehaviour
+{
+    FlowerSystem fs;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        /*
+        fs = FlowerManager.Instance.GetFlowerSystem("default");
+        
+        fs.SetupDialog();
+        fs.ReadTextFromResource("answer_question");
+        fs.RegisterCommand("lock",(List<string> _params)=>{   
+              
+        });
+
+        fs.RegisterCommand("release",(List<string> _params)=>{       
+            //balloon.GetComponent<balloon>().canPlay = false ;  
+            // 找到場景中的 balloon 對象
+            GameObject balloonObject = GameObject.Find("balloon"); // 確保名字與場景中的物件名稱一致
+            ballloon script = balloonObject.GetComponent<ballloon>();
+            script.canPlay = true;
+
+            GameObject balloonObject1 = GameObject.Find("balloon1"); // 確保名字與場景中的物件名稱一致
+            pink script1 = balloonObject1.GetComponent<pink>();
+            script1.canPlay = true;
+
+            GameObject balloonObject2 = GameObject.Find("balloon2"); // 確保名字與場景中的物件名稱一致
+            black script2 = balloonObject2.GetComponent<black>();
+            script2.canPlay = true;       
+                  
+        });
+        */
+
+    }
+    
+}
--- Assets/Scripts/npc/pink.cs
+++ Assets/Scripts/npc/pink.cs
@@ -8,24 +8,31 @@ public class pink : MonoBehaviour
     public int hp_max = 0;
     public GameObject blood;
     public Animator bulloonAni;
-
+    public bool canPlay = false;
+    private Rigidbody2D rb; // 參考 Rigidbody2D 組件
     
 
 
     void Start()
     {
         // 隨機生成 x 位置
-        float randomX = Random.Range(-7.85f, 7.85f);
+        float randomX = Random.Range(-7.85f, 2.24f);
         hp_max = 2; // 設置最大血量
         hp = hp_max; // 初始化當前血量
         
         // 設置新位置
-        this.transform.position = new Vector3(randomX, 4.28f, this.transform.position.z);
+        this.transform.position = new Vector3(randomX, 6.22f, this.transform.position.z);
+        rb = GetComponent<Rigidbody2D>();
+        rb.bodyType = RigidbodyType2D.Static;
         
     }
 
     void Update()
     {
+        if(canPlay == true)
+        {
+            rb.bodyType = RigidbodyType2D.Dynamic;
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -57,4 +64,6 @@ public class pink : MonoBehaviour
             Destroy(this.gameObject);
         }
     }
+
+    
 }
