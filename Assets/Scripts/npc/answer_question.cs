1bc15e004da0cc0bf8711c2da78de3c94b48a0af
WuRu918 <wu9189453@gmail.com>
2025-01-09 00:17:20 +0800

    Revert "問答畫面"
    This reverts commit b573bfd417b3d62cf40773f93e1e36cc7cac91a7.

--- Assets/Scripts/npc/answer_question.cs
+++ Assets/Scripts/npc/answer_question.cs
@@ -1,145 +1,47 @@
 using UnityEngine;
 using System.Collections;
-using UnityEngine.UI; // for Image component
-using TMPro; // for TMP_Text
+using System.Collections.Generic;
+using Flower;
+using UnityEngine.SceneManagement;
+using System;
 
 public class answer_question : MonoBehaviour
 {
-    public GameObject[] questionObjects; // 存放所有題目的 GameObject（每個題目對應一個 GameObject）
-    public int currentQuestionIndex = 0; // 目前題目的索引
-    public int score = 0; // 分数
-    public GameObject[] balloons; // 拖入所有气球的 prefab
-
-    public TMP_Text countdownText; // 倒数文字
-    public int countdownTime = 3; // 倒数秒数
-
+    FlowerSystem fs;
     void Start()
     {
-        StartCoroutine(GameLoop());
+
     }
 
-    IEnumerator GameLoop()
+    void Update()
     {
-        while (true)
-        {
-            // 如果題目索引超出範圍，表示遊戲結束
-            if (currentQuestionIndex >= questionSprites.Length)
-            {
-                countdownText.text = "遊戲結束";
-                yield return new WaitForSeconds(3f);
-                Debug.Log("遊戲結束，最終分數: " + score);
-                break; // 結束遊戲迴圈
-            }
+        /*
+        fs = FlowerManager.Instance.GetFlowerSystem("default");
+        
+        fs.SetupDialog();
+        fs.ReadTextFromResource("answer_question");
+        fs.RegisterCommand("lock",(List<string> _params)=>{   
+              
+        });
 
-            // 重置氣球
-            ResetBalloons();
+        fs.RegisterCommand("release",(List<string> _params)=>{       
+            //balloon.GetComponent<balloon>().canPlay = false ;  
+            // 找到場景中的 balloon 對象
+            GameObject balloonObject = GameObject.Find("balloon"); // 確保名字與場景中的物件名稱一致
+            ballloon script = balloonObject.GetComponent<ballloon>();
+            script.canPlay = true;
 
-            // 倒數計時
-            yield return StartCoroutine(StartCountdown());
+            GameObject balloonObject1 = GameObject.Find("balloon1"); // 確保名字與場景中的物件名稱一致
+            pink script1 = balloonObject1.GetComponent<pink>();
+            script1.canPlay = true;
 
-            // 等待玩家射擊時間
-            yield return new WaitForSeconds(8f);
+            GameObject balloonObject2 = GameObject.Find("balloon2"); // 確保名字與場景中的物件名稱一致
+            black script2 = balloonObject2.GetComponent<black>();
+            script2.canPlay = true;       
+                  
+        });
+        */
 
-            // 檢查是否射中
-            CheckResult();
-        }
     }
-
-    private IEnumerator StartCountdown()
-    {
-        countdownText.fontSize = 380;
-        countdownText.text = "Ready!";
-        yield return new WaitForSeconds(1f);
-
-        // 倒數計時
-        while (countdownTime > 0)
-        {
-            countdownText.fontSize = 500;
-            countdownText.text = countdownTime.ToString();
-            yield return new WaitForSeconds(1f);
-            countdownTime--;
-        }
-
-        countdownText.text = "Go!";
-        yield return new WaitForSeconds(1f);
-
-        // 清空倒數文字
-        countdownText.text = "";
-
-        // **按順序顯示題目圖片**
-        UpdateQuestionImage();
     
-        // 啟動氣球下落
-        EnableBalloonMovement();
-     
-    }
-
-    void UpdateQuestionObject()
-    {
-        // 確保上一題的 GameObject 隱藏
-        if (currentQuestionIndex > 0 && currentQuestionIndex - 1 < questionObjects.Length)
-        {
-            questionObjects[currentQuestionIndex - 1].SetActive(false);
-        }
-
-        // 顯示當前題目的 GameObject
-        questionObjects[currentQuestionIndex].SetActive(true);
-
-        // 更新索引
-        currentQuestionIndex++;
-    }
-    void ResetBalloons()
-    {
-        foreach (var balloon in balloons)
-        {
-            if (balloon == null) continue; // 確保氣球實例存在
-            var resettable = balloon.GetComponent<IResettable>();
-            if (resettable != null)
-            {
-                resettable.ResetBalloon(); // 重置氣球位置和狀態
-            }
-        }
-    }
-
-    void EnableBalloonMovement()
-    {
-        foreach (var balloon in balloons)
-        {
-            if (balloon == null) continue;
-            var resettable = balloon.GetComponent<IResettable>();
-            if (resettable != null)
-            {
-                ((MonoBehaviour)resettable).GetComponent<black>().canPlay = true; // 設置下落啟用
-            }
-        }
-    }
-
-    void CheckResult()
-    {
-        foreach (var balloon in balloons)
-        {
-            if (balloon == null) continue;
-
-            var resettable = balloon.GetComponent<IResettable>();
-            if (resettable != null && resettable.GetHP() <= 0) // 如果氣球被擊中
-            {
-                if (IsCorrectAnswer(balloon))
-                {
-                    score += 1;
-                }
-            }
-        }
-
-        Debug.Log("Current Score: " + score);
-    }
-
-    bool IsCorrectAnswer(GameObject balloon)
-    {
-        //獲取當前題目
-        string questionName = questionImage.sprite.name; 
-        //得到氣球標籤
-        string balloonTag = balloon.tag; 
-        //比較題目tag跟被射掉的氣球tag
-        return questionName == balloonTag;
-    }
 }
