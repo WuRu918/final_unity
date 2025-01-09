8eb3b5126c49c3c89a9d4bbc0b2ad57d5191bb9b
Winsw1228 <winstonlaipengfeng@gmail.com>
2025-01-10 03:40:20 +0800

    commit

---
+++ Assets/Scripts/npc/question_new.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using System;
+using TMPro; // for TMP_Text
+
+public class question_new: MonoBehaviour
+{
+    public GameObject text; // 顯示遊戲說明
+    bool isInRange = false;       // 判断玩家是否处于特定区域内
+
+
+
+    void Start()
+    {
+        this.transform.tag = "npc";       
+        // Collider 設為Trigger
+        Collider2D col = this.GetComponent<Collider2D>();
+        if (col != null && !col.isTrigger)
+        {
+            col.isTrigger = true; // 设置为触发器
+        }
+        text.SetActive(false);
+
+    }
+
+    void Update()
+    { 
+        if (isInRange) 
+        {
+            if(Input.GetKey(KeyCode.RightShift) || Input.GetKey(KeyCode.LeftShift))
+            {        
+                // 場景加載
+                SceneManager.LoadScene("answer_question");
+
+            }          
+        }
+        
+    }
+
+    // 进入触发区域时
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))  // 确保是玩家进入
+        {
+            // 玩家进入对话范围，启动对话
+            isInRange = true;
+            text.SetActive(true);
+        }
+    }
+
+    // 离开触发区域时
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))  // 确保是玩家离开
+        {
+            // 玩家離開範圍
+            isInRange = false;
+            text.SetActive(false);
+        }
+    }
+}
