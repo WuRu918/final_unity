7795bd9623f237afc84d2a06b66500e1d69096fa
WuRu918 <wu9189453@gmail.com>
2025-01-11 19:50:02 +0800

    要revert

--- Assets/Scripts/start/game_start.cs
+++ Assets/Scripts/start/game_start.cs
@@ -22,6 +22,7 @@ public class GameExplaination : MonoBehaviour
         yield return new WaitForSeconds(audio.clip.length);
 
         // 切換場景
+        Debug.Log("Switching Scene...");
         SceneManager.LoadScene("explaination");
     }
 }
